4d1bb6ce7f18f6fa29367011818fb2b0651bf206
UE-DND <100979820+UE-DND@users.noreply.github.com>
2026-01-15 01:21:18 +0800

    chore: 校验签名

--- src/parser.ts
+++ src/parser.ts
@@ -1,7 +1,6 @@
 export * from './core/ast';
 export * from './core/errors';
 export * from './core/parser';
-
 export * from './html/render/html';
 export * from './html/theme/theme';
 export * from './html/convert';
